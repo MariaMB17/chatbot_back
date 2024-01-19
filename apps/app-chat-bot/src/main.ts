597e9c25406958aa893e511eccc708288958f4b1
Ciberyely <ciberyely@hotmail.com>
2024-01-19 06:45:30 -0400

    add Cors

--- apps/app-chat-bot/src/main.ts
+++ apps/app-chat-bot/src/main.ts
@@ -1,17 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
+// import * as cors from 'cors';
 import * as session from 'express-session';
 import { AllExceptionFilter } from './allexceptionsfilter';
 import { AppModule } from './app.module';
 import { TransformationInterceptor } from './transformation.interceptor';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformationInterceptor(new Reflector()));
   app.useGlobalFilters(new AllExceptionFilter());
+
+  // app.use(cors({
+  //   origin: 'http://localhost:3000'
+  // }))
+
   app.use(
     session({
       secret: process.env.SECRET_KEY,
@@ -25,6 +31,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
 
-  await app.listen(port); 
+  await app.listen(port);
 }
 bootstrap();
